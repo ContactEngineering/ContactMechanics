3ca1453cf53eed5e077ede3e3a40841946cdc870
Lars Pastewka <lars.pastewka@kit.edu>
2016-04-25 14:41:34 +0200

    ENH: Added Greenwood-Tripp solution and tests. Added PyCoTestCase class that has array assertion methode.

---
+++ PyCo/ReferenceSolutions/GreenwoodTripp.py
@@ -0,0 +1,129 @@
+# -*- coding:utf-8 -*-
+"""
+@file   __init__.py
+
+@author Lars Pastewka <lars.pastewka@kit.edu>
+
+@date   25 April 2015
+
+@brief  Greenwood-Tripp model for the contact of rough spheres
+
+@section LICENCE
+
+ Copyright (C) 2015 Lars Pastewka
+
+PyCo is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3, or (at
+your option) any later version.
+
+PyCo is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING. If not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+import numpy as np
+from scipy.integrate import quad, simps
+from scipy.interpolate import interp1d
+from scipy.special import ellipe, ellipk
+# Note on notation in paper and scipy: K(x) = ellipk(x**2), E(x) = ellipe(x**2)
+
+###
+
+def _Fn(h, n, phi):
+    v, err = quad(lambda s: (s-h)**n * phi(s), h, np.inf)
+    return v
+
+def Fn(h, n, phi=lambda s: np.exp(-s**2/2)/((2*np.pi)**(1/2))):
+    """
+    Returns: int_h^infty (s-h)^n * phi(s) ds
+    """
+    if np.isscalar(h):
+        return _Fn(h, n, phi)
+    else:
+        return np.array([_Fn(_h, n, phi) for _h in h])
+
+
+# Use asymptotic expressions for small and large values
+def s(ξ, ξ1=1e-5, ξ2=1e5):
+    """
+    Returns: 2/pi*E(ξ)+(ξ^2-1)*K(ξ) if ξ<=1
+             2/pi*ξ*E(1/ξ) if ξ>1
+    Asymptotic cases are approximated by the asymptotic expressions.
+    """
+    B = lambda x: ellipe(x**2)+(x**2-1)*ellipk(x**2)
+    m1 = ξ < ξ1
+    m4 = ξ > ξ2
+    m = ξ < 1
+    m2 = np.logical_and(m, np.logical_not(m1))
+    m3 = np.logical_and(np.logical_not(m), np.logical_not(m4))
+    r = np.zeros_like(ξ)
+    r[m1] = ξ[m1]**2/2
+    r[m4] = ξ[m4]
+    r[m2] = 2/np.pi*B(ξ[m2])
+    r[m3] = 2/np.pi*ξ[m3]*ellipe(1/ξ[m3]**2)
+    return r
+
+
+# Use asymptotic expressions for small and large values
+def ξ(s, s1=1e-5, s2=1e5):
+    ξnum = lambda s0: brentq(lambda ξ: s(ξ)-s0, 1e-12, 1e12) if isscalar(s0) \
+        else array([brentq(lambda ξ: s(ξ)-_s0, 1e-12, 1e12) for _s0 in s0])
+    m1 = s < s1
+    m3 = s > s2
+    m2 = np.logical_and(np.logical_not(m1), np.logical_not(m3))
+    r = np.zeros_like(s)
+    r[m1] = sqrt(2*s[m1])
+    r[m3] = s[m3]
+    r[m2] = ξnum(s[m2])
+    return r
+
+
+def GreenwoodTripp(d, μ, rhomax=3, n=100, eps=1e-6):
+    """
+    Greenwood-Tripp solution for the contact of rough spheres.
+    See: Greenwood, Tripp, J. Appl. Mech. 34, 153 (1967)
+    Symbols correspond to notation in this paper.
+
+    Parameters
+    ----------
+    d : float
+        Dimensionless displacement d* of the sphere, d* = d / σ where σ is the
+        standard deviation of the height distribution (i.e. the rms height).
+    μ : float
+        Material parameter, μ=8/3 η σ sqrt(2 B β) where η is the density of
+        asperities, B is the sphere radius and β the asperity radius.
+
+    Returns
+    -------
+    w : array
+        Displacement in radial direction in units of σ.
+    p : array
+        Pressure in radial direction in units of E* sqrt(σ / 8B) where E* is
+        the contact modulus.
+    rho : array
+          Radial coordinate in units of sqrt(2 B σ)
+    """
+    ρ = np.linspace(0, rhomax, n)
+    w = np.zeros_like(rho)
+    w0 = 0
+    for i in range(10):
+        p = μ*Fn(d+ρ**2+w-w0, 3/2)
+        pint = interp1d(rho, p)
+        #print('p =', p)
+        w = np.zeros_like(ρ)
+        for i, _ρ in enumerate(ρ):
+            if i == 0:
+                w[i] = simps(p, x=ρ)
+            else:
+                ξ = np.linspace(0, (rhomax-eps)/_ρ, n)
+                #print((ξ*_rho).max())
+                w[i] = simps(_ρ*pint(_ρ*ξ), x=s(ξ))
+    p = μ*Fn(d+ρ**2+w-w0, 3/2)
+    return w, p, ρ
---
+++ tests/12-ReferenceSolutionsTest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+@file   11-GoodiesTest.py
+
+@author Lars Pastewka <lars.pastewka@kit.edu>
+
+@date   24 April 2016
+
+@brief  Tests for PyCo ReferenceSolutions
+
+@section LICENCE
+
+ Copyright (C) 2016 Lars Pastewka
+
+PyCo is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3, or (at
+your option) any later version.
+
+PyCo is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING. If not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+try:
+    import unittest
+    import numpy as np
+    import warnings
+    from scipy.integrate import quad
+    from scipy.special import ellipk, erf
+
+    from .pycotest import PyCoTestCase
+    import PyCo.ReferenceSolutions.GreenwoodTripp as GT
+except ImportError as err:
+    import sys
+    print(err)
+    sys.exit(-1)
+
+class ReferenceSolutionsTest(PyCoTestCase):
+    def test_Fn(self):
+        x = np.linspace(-3, 3, 101)
+        f = GT.Fn(x, 0)
+        self.assertArrayAlmostEqual(GT.Fn(x, 0), (1-erf(x/np.sqrt(2)))/2)
+    def test_s(self):
+        L = lambda x: np.where(x<=1, 2/np.pi*x*ellipk(x**2), 2/np.pi*ellipk(1/x**2))
+        snum = lambda ξ: np.array([quad(L, 0, _ξ)[0] for _ξ in ξ])
+
+        x = np.linspace(0.01, 5, 101)
+        self.assertArrayAlmostEqual(GT.s(x), snum(x))
