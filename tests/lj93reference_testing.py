b52136f697e8588f24ef64aa4ff0fd85957b0765
Lars Pastewka <lars.pastewka@kit.edu>
2016-07-05 14:56:10 +0200

    MAINT: Removed another unused matplotlib import.

--- tests/lj93reference_testing.py
+++ tests/lj93reference_testing.py
@@ -3,7 +3,6 @@
 from . import lj93_ref_potential as lj93
 from . import lj93smooth_ref_potential as lj93s
 import numpy as np
-import matplotlib.pyplot as plt
 
 eps = 1.
 sig = 1.
@@ -18,6 +17,7 @@ x = np.arange(xmin, xmax, step)
 v  = lj93. V(x, eps, sig, rc1)
 vs = lj93s.V(x, eps, sig, 1.1, rc1)
 
+#import matplotlib.pyplot as plt
 ## plt.plot(x, v,  label='std')
 ## plt.plot(x, vs, label='smooth')
 ## plt.legend(loc='best')
