0c18abfcdf8fe5ae690e265c16639d104c43df57
Lars Pastewka <lars.pastewka@kit.edu>
2016-10-21 11:04:55 +0200

    BUG: Added argparse import. Also: Added license header.

--- commandline/plotmap.py
+++ commandline/plotmap.py
@@ -1,4 +1,34 @@
 #! /usr/bin/env python3
+"""
+@file   plotmap.py
+
+@author Lars Pastewka <lars.pastewka@kit.edu>
+
+@date   20 Oct 2016
+
+@brief  Command line front-end for plotting 2D surfaces
+
+@section LICENCE
+
+ Copyright (C) 2016 Lars Pastewka
+
+PyCo is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3, or (at
+your option) any later version.
+
+PyCo is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING. If not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+from argparse import ArgumentParser, ArgumentTypeError
 
 import matplotlib.pyplot as plt
 
@@ -9,6 +39,7 @@ from PyCo.Surface import read
 parser = ArgumentParser(description='Plot a 2D surface map (pressure, gap, '
                                     'etc.).')
 parser.add_argument('filename', metavar='FILENAME', help='name of map file')
+arguments = parser.parse_args()
 
 ###
 
@@ -18,12 +49,13 @@ surface = read(arguments.filename)
 
 try:
     sx, sy = surface.size
-except ValueError:
+except TypeError:
     sx, sy = surface.shape
 
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect=sx/sy)
-ax.pcolormesh(surface[...])
+mesh = ax.pcolormesh(surface[...])
+plt.colorbar(mesh, ax=ax)
 ax.set_xlim(0, sx)
 ax.set_ylim(0, sy)
 if surface.unit is not None:
