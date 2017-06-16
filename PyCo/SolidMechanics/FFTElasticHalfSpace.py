BUG: Avoid numerical overflow for substrates of large thickness. Making sure that expression converges to infinite substrate result. Added test for this.

PyCo/SolidMechanics/FFTElasticHalfSpace.py
@@ -180,8 +180,12 @@ class PeriodicFFTElasticHalfSpace(ElasticSubstrate):
                 q *= 2*np.pi*self.thickness
                 fac = 3 - 4*self.poisson
                 off = 4*self.poisson*(2*self.poisson - 3) + 5
-                facts *= (fac*np.cosh(2*q) + 2*q**2 + off)/ \
+                corr = (fac*np.cosh(2*q) + 2*q**2 + off)/ \
                     (fac*np.sinh(2*q) - 2*q)
+                # The expression easily overflows numerically. These are then
+                # q-values that are converged to the infinite system expression.
+                corr[np.isnan(corr)] = 1.0
+                facts *= corr
                 facts[0, 0] = self.young/self.thickness * \
                     (1 - self.poisson)/((1 - 2*self.poisson)*(1 + self.poisson))
             else:

tests/03-FFTElasticHalfSpaceTest.py
@@ -285,7 +285,14 @@ class PeriodicFFTElasticHalfSpaceTest(PyCoTestCase):
         hsf = PeriodicFFTElasticHalfSpace(self.res, self.young, self.size,
                                           poisson=self.poisson, thickness=20)
         diff = hs.weights-hsf.weights
-        self.assertArrayAlmostEqual(hs.weights.ravel()[1:], hsf.weights.ravel()[1:])
+        self.assertArrayAlmostEqual(hs.weights.ravel()[1:],
+                                    hsf.weights.ravel()[1:], tol=1e-6)
+
+    def test_no_nans(self):
+        hs = PeriodicFFTElasticHalfSpace(self.res, self.young, self.size,
+                                         poisson=self.poisson, thickness=100)
+        self.assertTrue(np.count_nonzero(np.isnan(hs.weights)) == 0)
+
 
 class FreeFFTElasticHalfSpaceTest(unittest.TestCase):
     def setUp(self):