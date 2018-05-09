a642b2e23d5303203d50d98b34441d2679852794
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2018-05-09 22:44:58 +0200

    ENH: Added option to compute nonperiodic 2D autocorrelation.

--- PyCo/Tools/Autocorrelation.py
+++ PyCo/Tools/Autocorrelation.py
@@ -34,9 +34,11 @@ SOFTWARE.
 
 import numpy as np
 
+from .common import _get_size, radial_average
+
 
 def autocorrelation_2D(surface_xy, nbins=100,  # pylint: disable=invalid-name
-                       size=None, return_map=False):
+                       size=None, periodic=False, return_map=False):
     """
     Compute height-difference autocorrelation function and radial average.
 
@@ -50,15 +52,20 @@ def autocorrelation_2D(surface_xy, nbins=100,  # pylint: disable=invalid-name
     size : (float, float), optional
         Physical size of the 2D grid. (Default: Size is equal to number of grid
         points.)
+    periodic : bool, optional
+        Surface is periodic topography map. (Default: False)
     return_map : bool, optional
-        Return full 2D power spectrum map. (Default: False)
+        Return full 2D autocorrelation map. (Default: False)
 
     Returns
     -------
-    r : array_like
-        Distances.
-    A : array_like
+    r : array
+        Distances. (Units: length)
+    A : array
         Autocorrelation function. (Units: length**2)
+    A_xy : array
+        2D autocorrelation function. Only returned if return_map=True.
+        (Units: length**2)
     """
     nx, ny = surface_xy.shape
     sx, sy = _get_size(surface_xy, size)
@@ -67,16 +74,38 @@ def autocorrelation_2D(surface_xy, nbins=100,  # pylint: disable=invalid-name
     area0 = (sx/nx)*(sy/ny)
 
     # Compute FFT and normalize
-    surface_qk = np.fft.fft2(surface_xy[:, :])
-    C_qk = abs(surface_qk)**2  # pylint: disable=invalid-name
-    A_xy = np.fft.ifft2(C_qk).real/(nx*ny)
+    if periodic:
+        surface_qk = np.fft.fft2(surface_xy[:, :])
+        C_qk = abs(surface_qk)**2  # pylint: disable=invalid-name
+        A_xy = np.fft.ifft2(C_qk).real/(nx*ny)
 
-    if nbins is None:
-        return A_xy
+        # Convert height-height autocorrelation to height-difference
+        # autocorrelation
+        A_xy = A_xy[0, 0]-A_xy
 
-    # Radial average
-    r_edges, n, r_val, A_val = radial_average(  # pylint: disable=invalid-name
-        A_xy[0, 0]-A_xy, (sx+sy)/4, nbins, size=(sx, sy))
+        if nbins is None:
+            return A_xy
+
+        # Radial average
+        r_edges, n, r_val, A_val = radial_average(  # pylint: disable=invalid-name
+            A_xy, (sx+sy)/4, nbins, size=(sx, sy))
+    else:
+        surface_qk = np.fft.fft2(surface_xy[:, :], s=(2*nx-1, 2*ny-1))
+        C_qk = abs(surface_qk)**2  # pylint: disable=invalid-name
+        normx = np.abs(np.arange(2*nx-1) - nx).reshape(-1, 1)
+        normy = np.abs(np.arange(2*ny-1) - ny).reshape(1, -1)
+        A_xy = np.fft.ifft2(C_qk).real/(normx*normy)
+
+        # Convert height-height autocorrelation to height-difference
+        # autocorrelation
+        A_xy = A_xy[0, 0]-A_xy
+
+        if nbins is None:
+            return A_xy
+
+        # Radial average
+        r_edges, n, r_val, A_val = radial_average(  # pylint: disable=invalid-name
+            A_xy, (sx+sy)/2, nbins, size=(sx*(2*nx-1)/nx, sy*(2*ny-1)/ny))
 
     if return_map:
         return r_val[n>0], A_val[n>0], A_xy
