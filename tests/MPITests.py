475baf52c73f9c4841033db0d317a73494c269a0
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2018-10-31 14:26:29 +0100

    TST: FreeFFTElasticHalfSpace: test of the calculation of the weights and test for the rectangular Pressure

--- tests/MPITests.py
+++ tests/MPITests.py
@@ -18,6 +18,7 @@ if _withMPI:
 
 from PyCo.SolidMechanics import PeriodicFFTElasticHalfSpace
 from PyCo.SolidMechanics import FreeFFTElasticHalfSpace
+from .PyCoTest import PyCoTestCase
 
 from FFTEngine import NumpyFFTEngine
 DEFAULTFFTENGINE = NumpyFFTEngine
@@ -126,7 +127,7 @@ class test_ParallelNumpy(unittest.TestCase):
 
 
 @unittest.skipUnless(_withMPI,"requires mpi4py")
-class Parallel_FFTElasticHalfSpace_weights(unittest.TestCase):
+class test_FFTElasticHalfSpace_weights(unittest.TestCase):
     """
 
     """
@@ -172,7 +173,7 @@ class Parallel_FFTElasticHalfSpace_weights(unittest.TestCase):
             np.testing.assert_allclose(reference.weights[substrate.fourier_slice], substrate.weights, rtol=0, atol=1e-16,err_msg="weights are different")
             np.testing.assert_allclose(reference.iweights[substrate.fourier_slice], substrate.iweights, rtol=0, atol=1e-16, err_msg="iweights are different")
 
-class Parallel_FFTElasticHalfSpace_compute(unittest.TestCase):
+class test_FFTElasticHalfSpace_compute(unittest.TestCase):
     def setUp(self):
         self.sx = 2  # 30.0
         self.sy = 1.0
@@ -313,7 +314,134 @@ class Parallel_FFTElasticHalfSpace_compute(unittest.TestCase):
         pass
 
 @unittest.skipUnless(_withMPI,"requires mpi4py")
-class Parallel_FreeFFTElasticHalfSpace(unittest.TestCase):
+class test_FreeFFTElasticHalfSpace(PyCoTestCase):
+    def setUp(self):
+        self.E_s = 3
+        self.fftengineList= [PFFTEngine]
+        self.comm = MPI.COMM_WORLD
+        self.pnp = ParallelNumpy(self.comm)
+
+    def test_resolutions(self):
+        nx,ny = 32,16
+        sx,sy = 100,200
+
+        for fftengine in self.fftengineList:
+            with self.subTest(fftengine=fftengine):
+                substrate=FreeFFTElasticHalfSpace((nx,ny),self.E_s,(sx,sy),fftengine=fftengine)
+                self.assertEqual(substrate.resolution,(nx,ny))
+                self.assertEqual(substrate.domain_resolution, (2*nx, 2*ny))
+
+                self.assertEqual(self.pnp.sum(np.array(np.prod(substrate.subdomain_resolution))),4*nx*ny)
+
+    def test_weights(self):
+        def _compute_fourier_coeffs_serial_impl(self):
+            """Compute the weights w relating fft(displacement) to fft(pressure):
+               fft(u) = w*fft(p), Johnson, p. 54, and Hockney, p. 178
+
+               This version is less is copied from matscipy, use if memory is a
+               concern
+            """
+            # pylint: disable=invalid-name
+            facts = np.zeros(tuple((res * 2 for res in self.resolution)))
+            a = self.steps[0] * .5
+            if self.dim == 1:
+                pass
+            else:
+                b = self.steps[1] * .5
+                x_s = np.arange(self.resolution[0] * 2)
+                x_s = np.where(x_s <= self.resolution[0], x_s,
+                               x_s - self.resolution[0] * 2) * self.steps[0]
+                x_s.shape = (-1, 1)
+                y_s = np.arange(self.resolution[1] * 2)
+                y_s = np.where(y_s <= self.resolution[1], y_s,
+                               y_s - self.resolution[1] * 2) * self.steps[1]
+                y_s.shape = (1, -1)
+                facts = 1 / (np.pi * self.young) * (
+                        (x_s + a) * np.log(((y_s + b) + np.sqrt((y_s + b) * (y_s + b) +
+                                                                (x_s + a) * (x_s + a))) /
+                                           ((y_s - b) + np.sqrt((y_s - b) * (y_s - b) +
+                                                                (x_s + a) * (x_s + a)))) +
+                        (y_s + b) * np.log(((x_s + a) + np.sqrt((y_s + b) * (y_s + b) +
+                                                                (x_s + a) * (x_s + a))) /
+                                           ((x_s - a) + np.sqrt((y_s + b) * (y_s + b) +
+                                                                (x_s - a) * (x_s - a)))) +
+                        (x_s - a) * np.log(((y_s - b) + np.sqrt((y_s - b) * (y_s - b) +
+                                                                (x_s - a) * (x_s - a))) /
+                                           ((y_s + b) + np.sqrt((y_s + b) * (y_s + b) +
+                                                                (x_s - a) * (x_s - a)))) +
+                        (y_s - b) * np.log(((x_s - a) + np.sqrt((y_s - b) * (y_s - b) +
+                                                                (x_s - a) * (x_s - a))) /
+                                           ((x_s + a) + np.sqrt((y_s - b) * (y_s - b) +
+                                                                (x_s + a) * (x_s + a)))))
+            self.weights = np.fft.rfftn(facts)
+            return self.weights, facts
+
+        nx, ny = 32, 16
+        sx, sy = 100, 200
+
+        ref_weights, ref_facts = _compute_fourier_coeffs_serial_impl(
+        FreeFFTElasticHalfSpace((nx, ny), self.E_s, (sx, sy), fftengine=NumpyFFTEngine))
+
+        for fftengine in self.fftengineList:
+            with self.subTest(fftengine=fftengine):
+                substrate=FreeFFTElasticHalfSpace((nx,ny),self.E_s,(sx,sy),fftengine=fftengine)
+                local_weights,local_facts = substrate._compute_fourier_coeffs()
+                np.testing.assert_allclose(local_weights,ref_weights[substrate.fourier_slice],1e-14)
+                np.testing.assert_allclose(local_facts,ref_facts[substrate.subdomain_slice],1e-14)
+
+    def test_evaluate_disp_uniform_pressure(self):
+        nx, ny = 32, 16
+        sx, sy = 100, 200
+
+        forces = np.zeros((2 * nx, 2 * ny))
+        x = (np.arange(2 * nx) - (nx - 1) / 2) * sx / nx
+        x.shape = (-1, 1)
+        y = (np.arange(2 * ny) - (ny - 1) / 2) * sy / ny
+        y.shape = (1, -1)
+
+        forces[1:nx - 1, 1:ny - 1] = -np.ones((nx - 2, ny - 2)) * (sx * sy) / (nx * ny)
+        a = (nx - 2) / 2 * sx / nx
+        b = (ny - 2) / 2 * sy / ny
+        refdisp = 1/(np.pi*self.E_s) * (
+                (x+a)*np.log(((y+b)+np.sqrt((y+b)*(y+b) +
+                                                (x+a)*(x+a))) /
+                               ((y-b)+np.sqrt((y-b)*(y-b) +
+                                                (x+a)*(x+a)))) +
+                (y+b)*np.log(((x+a)+np.sqrt((y+b)*(y+b) +
+                                                (x+a)*(x+a))) /
+                               ((x-a)+np.sqrt((y+b)*(y+b) +
+                                                (x-a)*(x-a)))) +
+                (x-a)*np.log(((y-b)+np.sqrt((y-b)*(y-b) +
+                                                (x-a)*(x-a))) /
+                               ((y+b)+np.sqrt((y+b)*(y+b) +
+                                                (x-a)*(x-a)))) +
+                (y-b)*np.log(((x-a)+np.sqrt((y-b)*(y-b) +
+                                                (x-a)*(x-a))) /
+                               ((x+a)+np.sqrt((y-b)*(y-b) +
+                                                (x+a)*(x+a)))))
+
+        for fftengine in self.fftengineList:
+            with self.subTest(fftengine=fftengine):
+                substrate = FreeFFTElasticHalfSpace((nx, ny), self.E_s, (sx, sy), fftengine=PFFTEngine)
+
+                if self.comm.Get_size() > 1:
+                    with self.assertRaises(FreeFFTElasticHalfSpace.Error):
+                        substrate.evaluate_disp(forces)
+                    with self.assertRaises(FreeFFTElasticHalfSpace.Error):
+                        substrate.evaluate_disp(forces[nx, ny])
+
+                #print(forces[substrate.subdomain_slice])
+                computed_disp = substrate.evaluate_disp(forces[substrate.subdomain_slice])
+                #print(computed_disp)
+                # make the comparison only on the nonpadded domain
+                s_c = tuple([slice(1, max(0, min(substrate.resolution[i]-1 - substrate.subdomain_location[i], substrate.subdomain_resolution[i])))
+                         for i in range(substrate.dim)])
+
+                s_refdisp = tuple([slice(s_c[i].start + substrate.subdomain_location[i], s_c[i].stop + substrate.subdomain_location[i]) for i in range(substrate.dim)])
+                #print(s_c)
+                #print(s_refdisp)
+                np.testing.assert_allclose(computed_disp[s_c],refdisp[s_refdisp])
+
 
     def test_Parabolic_Displacement(self):
         """
@@ -334,3 +462,5 @@ class Parallel_FreeFFTElasticHalfSpace(unittest.TestCase):
         """
         pass
 
+if __name__ == '__main__':
+    unittest.main()
