WIP: Refactored TopographyBase.py into TopographyUniform.py and TopographyNonuniform.py. Fixed computation of scalar roughness parameters

tests/ToolsTest.py
@@ -40,7 +40,7 @@ try:
     import PyCo.Topography.Nonuniform as Nonuniform
     import PyCo.Topography.Uniform as Uniform
     from PyCo.Tools import evaluate_gradient, mean_err
-    from PyCo.Topography import (autocorrelation_1D, autocorrelation_2D, compute_derivative, tilt_from_height,
+    from PyCo.Topography import (autocorrelation_1D, autocorrelation_2D, tilt_from_height,
                                  shift_and_tilt, shift_and_tilt_approx, shift_and_tilt_from_slope,
                                  NonuniformNumpyTopography, UniformNumpyTopography)
     from PyCo.Topography.Generation import RandomSurfaceGaussian, RandomSurfaceExact
@@ -99,9 +99,9 @@ class ToolTest(PyCoTestCase):
         self.assertTrue(error < tol, "error = {}, tol = {}, arr_out = {}".format(
             error, tol, arr_approx))
 
-        mean_slope = [x.mean() for x in compute_derivative(arr)]
+        mean_slope = [np.diff(arr, axis=d).mean() for d in range(len(arr.shape))]
         arr_out = shift_and_tilt_from_slope(arr)
-        mean_slope = [x.mean() for x in compute_derivative(arr_out)]
+        mean_slope = [np.diff(arr_out, axis=d).mean() for d in range(len(arr_out.shape))]
         self.assertAlmostEqual(mean_slope[0], 0)
         self.assertAlmostEqual(mean_slope[1], 0)
 

tests/TopographyScalarParametersTest.py
@@ -4,6 +4,7 @@ try:
     import time
     import math
 
+    from PyCo.Topography import UniformNumpyTopography, NonuniformNumpyTopography
     import PyCo.Topography.Uniform.ScalarParameters as Uniform
     import PyCo.Topography.Nonuniform.ScalarParameters as Nonuniform
 
@@ -23,22 +24,24 @@ class SinewaveTestUniform(unittest.TestCase):
         self.sinsurf = np.sin(2 * np.pi / self.L * X) * np.sin(2 * np.pi / self.L * Y) * self.hm
         self.size= (self.L,self.L)
 
+        self.surf = UniformNumpyTopography(self.sinsurf, size=self.size)
+
         self.precision = 5
 
     def test_rms_curvature(self):
-        numerical = Uniform.rms_curvature(self.sinsurf, size=self.size)
+        numerical = self.surf.rms_curvature()
         analytical = np.sqrt(16*np.pi**4 *self.hm**2 / self.L**4 )
         #print(numerical-analytical)
         self.assertAlmostEqual(numerical,analytical,self.precision)
 
     def test_rms_slope(self):
-        numerical = Uniform.rms_slope(self.sinsurf, size=self.size)
+        numerical = self.surf.rms_slope()
         analytical = np.sqrt(2*np.pi ** 2 * self.hm**2 / self.L**2)
         # print(numerical-analytical)
         self.assertAlmostEqual(numerical, analytical, self.precision)
 
     def test_rms_height(self):
-        numerical = Uniform.rms_height(self.sinsurf )
+        numerical = self.surf.rms_height()
         analytical = np.sqrt(self.hm**2 / 4)
 
         self.assertEqual(numerical,analytical)