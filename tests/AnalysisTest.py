fcf78a1d0ced28226c4ef142ab4085ed04f14a1c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2018-12-18 22:06:10 +0100

    TST: Test now runs for periodic and nonperiodic topographies

--- tests/AnalysisTest.py
+++ tests/AnalysisTest.py
@@ -42,22 +42,26 @@ from tests.PyCoTest import PyCoTestCase
 
 class PowerSpectrumTest(PyCoTestCase):
     def test_uniform(self):
-        for L in [1.3, 10.6]:
-            for k in [2, 4]:
-                for n in [16, 128]:
-                    n = 16
-                    x = np.arange(n)*L/n
-                    h = np.sin(2*np.pi*k*x/L)
-                    t = UniformNumpyTopography(h, size=(L,), periodic=True)
-                    q, C = t.power_spectrum_1D()
+        for periodic in [True, False]:
+            for L in [1.3, 10.6]:
+                for k in [2, 4]:
+                    for n in [16, 128]:
+                        n = 16
+                        x = np.arange(n)*L/n
+                        h = np.sin(2*np.pi*k*x/L)
+                        t = UniformNumpyTopography(h, size=(L,), periodic=periodic)
+                        q, C = t.power_spectrum_1D()
 
-                    # The ms height of the sine is 1/2. The sum over the PSD (from -q to +q) is the ms height.
-                    # Our PSD only contains *half* of the full PSD (on the +q branch, the -q branch is identical),
-                    # therefore the sum over it is 1/4.
-                    self.assertAlmostEqual(C.sum()/L, 1/4)
+                        # The ms height of the sine is 1/2. The sum over the PSD (from -q to +q) is the ms height.
+                        # Our PSD only contains *half* of the full PSD (on the +q branch, the -q branch is identical),
+                        # therefore the sum over it is 1/4.
+                        self.assertAlmostEqual(C.sum()/L, 1/4)
 
-                    # The value at the individual wavevector must also equal 1/4
-                    C /= L
-                    r = np.zeros_like(C)
-                    r[k] = 1/4
-                    self.assertArrayAlmostEqual(C, r)
+                        if periodic:
+                            # The value at the individual wavevector must also equal 1/4. This is only exactly true
+                            # for the periodic case. In the nonperiodic, this is convolved with the Fourier transform
+                            # of the window function.
+                            C /= L
+                            r = np.zeros_like(C)
+                            r[k] = 1/4
+                            self.assertArrayAlmostEqual(C, r)
