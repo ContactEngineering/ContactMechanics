46fe2de11feae3ca9898513040b3136992c2b6ba
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-02-23 20:42:10 +0100

    MAINT: Moved autocorrelation tests from ToolsTest to AnalysisTest, use methods now (rather than calling function directly)

--- tests/ToolsTest.py
+++ tests/ToolsTest.py
@@ -32,26 +32,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-try:
-    import unittest
-    import numpy as np
-    import warnings
+import numpy as np
 
-    import PyCo.Topography.Nonuniform as Nonuniform
-    import PyCo.Topography.Uniform as Uniform
-    from PyCo.Tools import evaluate_gradient, mean_err
-    from PyCo.Tools.ContactAreaAnalysis import distance_map
-    from PyCo.Topography import Topography, UniformLineScan, NonuniformLineScan
-    from PyCo.Topography.Generation import RandomSurfaceGaussian, RandomSurfaceExact
-    from PyCo.Topography.Nonuniform.Detrending import polyfit
-    from PyCo.Topography.Uniform.Autocorrelation import autocorrelation_1D, autocorrelation_2D
-    from PyCo.Topography.Uniform.Detrending import tilt_from_height, shift_and_tilt
+from PyCo.Tools import evaluate_gradient, mean_err
+from PyCo.Tools.ContactAreaAnalysis import distance_map
+from PyCo.Topography import Topography, UniformLineScan, NonuniformLineScan
+from PyCo.Topography.Nonuniform.Detrending import polyfit
+from PyCo.Topography.Uniform.Detrending import tilt_from_height, shift_and_tilt
 
-    from .PyCoTest import PyCoTestCase
-except ImportError as err:
-    import sys
-    print(err)
-    sys.exit(-1)
+from .PyCoTest import PyCoTestCase
+
 
 class ToolTest(PyCoTestCase):
     def test_gradient(self):
@@ -101,63 +91,6 @@ class ToolTest(PyCoTestCase):
         self.assertAlmostEqual(mean_slope[0], b*nx)
         self.assertAlmostEqual(mean_slope[1], a*ny)
         self.assertAlmostEqual(mean_slope[2], d)
-
-
-    def test_impulse_autocorrelation(self):
-        nx = 16
-        for x, w, h, p in [(nx//2, 3, 1, True), (nx//3, 2, 2, True),
-                           (nx//2, 5, 1, False), (nx//3, 6, 2.5, False)]:
-            y = np.zeros(nx)
-            y[x-w//2:x+(w+1)//2] = h
-            r, A = autocorrelation_1D(UniformLineScan(y, nx, periodic=True))
-
-            A_ana = np.zeros_like(A)
-            A_ana[:w] = h**2*np.linspace(w/nx, 1/nx, w)
-            A_ana = A_ana[0] - A_ana
-            self.assertTrue(np.allclose(A, A_ana))
-
-
-    def test_brute_force_autocorrelation_1D(self):
-        n = 10
-        for surf in [UniformLineScan(np.ones(n), n, periodic=False),
-                     UniformLineScan(np.arange(n), n, periodic=False),
-                     Topography(np.random.random(n).reshape(n, 1), (n, 1), periodic=False)]:
-            r, A = autocorrelation_1D(surf)
-
-            n = len(A)
-            dir_A = np.zeros(n)
-            for d in range(n):
-                for i in range(n-d):
-                    dir_A[d] += (surf.heights()[i] - surf.heights()[i+d])**2/2
-                dir_A[d] /= (n-d)
-            self.assertArrayAlmostEqual(A, dir_A)
-
-
-    def test_brute_force_autocorrelation_2D(self):
-        n = 10
-        m = 11
-        for surf in [Topography(np.ones([n, m]), (n, m), periodic=False),
-                     Topography(np.random.random([n, m]), (n, m), periodic=False)]:
-            r, A, A_xy = autocorrelation_2D(surf, return_map=True)
-
-            nx, ny = surf.resolution
-            dir_A_xy = np.zeros([n, m])
-            dir_A = np.zeros_like(A)
-            dir_n = np.zeros_like(A)
-            for dx in range(n):
-                for dy in range(m):
-                    for i in range(nx-dx):
-                        for j in range(ny-dy):
-                            dir_A_xy[dx, dy] += (surf.heights()[i, j] - surf.heights()[i+dx, j+dy])**2/2
-                    dir_A_xy[dx, dy] /= (nx-dx)*(ny-dy)
-                    d = np.sqrt(dx**2 + dy**2)
-                    i = np.argmin(np.abs(r-d))
-                    dir_A[i] += dir_A_xy[dx, dy]
-                    dir_n[i] += 1
-            dir_n[dir_n==0] = 1
-            dir_A /= dir_n
-            self.assertArrayAlmostEqual(A_xy, dir_A_xy)
-            self.assertArrayAlmostEqual(A[:-2], dir_A[:-2])
 
 
     def test_nonuniform_rms_height(self):
