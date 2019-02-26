3b4b36d4e50d9ca11bf4e9002bc19e2edb9f065f
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2019-02-26 08:37:15 +0100

    WIP: removed ugly hardcoded fix for dependencies

--- PyCo/SolidMechanics/FFTElasticHalfSpace.py
+++ PyCo/SolidMechanics/FFTElasticHalfSpace.py
@@ -31,7 +31,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
+import sys
+sys.path.append("/Users/antoines/MPITools")
+sys.path.append("/Users/antoines/FFTEngine")
 from collections import namedtuple
 import numpy as np
 import sys
