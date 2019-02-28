e48f12fb561dfea2c5e9c90fd8373d472ad14ed7
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-02-28 23:05:47 +0100

    MAINT: Removed debug plot output; pep-8

--- tests/AutocorrelationTest.py
+++ tests/AutocorrelationTest.py
@@ -42,40 +42,33 @@ from tests.PyCoTest import PyCoTestCase
 class AutocorrelationTest(PyCoTestCase):
     def test_impulse_autocorrelation(self):
         nx = 16
-        for x, w, h, p in [(nx//2, 3, 1, True), (nx//3, 2, 2, True),
-                           (nx//2, 5, 1, False), (nx//3, 6, 2.5, False)]:
+        for x, w, h, p in [(nx // 2, 3, 1, True), (nx // 3, 2, 2, True),
+                           (nx // 2, 5, 1, False), (nx // 3, 6, 2.5, False)]:
             y = np.zeros(nx)
-            y[x-w//2:x+(w+1)//2] = h
+            y[x - w // 2:x + (w + 1) // 2] = h
             r, A = UniformLineScan(y, nx, periodic=True).autocorrelation_1D()
 
             A_ana = np.zeros_like(A)
-            A_ana[:w] = h**2*np.linspace(w/nx, 1/nx, w)
+            A_ana[:w] = h ** 2 * np.linspace(w / nx, 1 / nx, w)
             A_ana = A_ana[0] - A_ana
             self.assertTrue(np.allclose(A, A_ana))
 
-
     def test_nonuniform_impulse_autocorrelation(self):
         x = np.array([0, 3.])
-        t = NonuniformLineScan(x, 2*np.ones_like(x))
+        t = NonuniformLineScan(x, 2 * np.ones_like(x))
         r, A = t.autocorrelation_1D(distances=np.linspace(-4, 4, 101))
-
-        import matplotlib.pyplot as plt
-        plt.figure()
-        plt.plot(*t.positions_and_heights(), 'r-')
-        plt.plot(r, A, 'k-')
-        plt.show()
 
     def test_nonuniform_triangle_autocorrelation(self):
         a = 0.7
         b = 3
         x = np.array([0, b])
-        t = NonuniformLineScan(x, a*x)
+        t = NonuniformLineScan(x, a * x)
         r, A = t.autocorrelation_1D(distances=np.linspace(0, 4, 101))
 
-        self.assertAlmostEqual(A[np.abs(r)<1e-6][0], a**2*b**3/3)
+        self.assertAlmostEqual(A[np.abs(r) < 1e-6][0], a ** 2 * b ** 3 / 3)
 
         x = np.array([0, 1., 1.3, 1.7, 2.0, 2.5, 3.0])
-        t = NonuniformLineScan(x, 0.7*x)
+        t = NonuniformLineScan(x, 0.7 * x)
         r2, A2 = t.autocorrelation_1D(distances=np.linspace(0, 4, 101))
 
         self.assertArrayAlmostEqual(A, A2)
@@ -90,11 +83,10 @@ class AutocorrelationTest(PyCoTestCase):
             n = len(A)
             dir_A = np.zeros(n)
             for d in range(n):
-                for i in range(n-d):
-                    dir_A[d] += (surf.heights()[i] - surf.heights()[i+d])**2/2
-                dir_A[d] /= (n-d)
+                for i in range(n - d):
+                    dir_A[d] += (surf.heights()[i] - surf.heights()[i + d]) ** 2 / 2
+                dir_A[d] /= (n - d)
             self.assertArrayAlmostEqual(A, dir_A)
-
 
     def test_brute_force_autocorrelation_2D(self):
         n = 10
@@ -109,15 +101,15 @@ class AutocorrelationTest(PyCoTestCase):
             dir_n = np.zeros_like(A)
             for dx in range(n):
                 for dy in range(m):
-                    for i in range(nx-dx):
-                        for j in range(ny-dy):
-                            dir_A_xy[dx, dy] += (surf.heights()[i, j] - surf.heights()[i+dx, j+dy])**2/2
-                    dir_A_xy[dx, dy] /= (nx-dx)*(ny-dy)
-                    d = np.sqrt(dx**2 + dy**2)
-                    i = np.argmin(np.abs(r-d))
+                    for i in range(nx - dx):
+                        for j in range(ny - dy):
+                            dir_A_xy[dx, dy] += (surf.heights()[i, j] - surf.heights()[i + dx, j + dy]) ** 2 / 2
+                    dir_A_xy[dx, dy] /= (nx - dx) * (ny - dy)
+                    d = np.sqrt(dx ** 2 + dy ** 2)
+                    i = np.argmin(np.abs(r - d))
                     dir_A[i] += dir_A_xy[dx, dy]
                     dir_n[i] += 1
-            dir_n[dir_n==0] = 1
+            dir_n[dir_n == 0] = 1
             dir_A /= dir_n
             self.assertArrayAlmostEqual(A_xy, dir_A_xy)
             self.assertArrayAlmostEqual(A[:-2], dir_A[:-2])
