ead2a3e16d733eb125d551324998fe6006da6d7c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-02-28 23:34:28 +0100

    BUG: Fixed 1D autocorrelation for negative distances MAINT: Created a separate function for the height-height autocorrelation functions (as opposed to the height-difference autocorrelation function)

--- tests/AutocorrelationTest.py
+++ tests/AutocorrelationTest.py
@@ -35,12 +35,14 @@ SOFTWARE.
 import numpy as np
 
 from PyCo.Topography import Topography, UniformLineScan, NonuniformLineScan
+from PyCo.Topography.Generation import fourier_synthesis
+from PyCo.Topography.Nonuniform.Autocorrelation import _bare_autocorrelation_1D
 
 from tests.PyCoTest import PyCoTestCase
 
 
 class AutocorrelationTest(PyCoTestCase):
-    def test_impulse_autocorrelation(self):
+    def test_uniform_impulse_autocorrelation(self):
         nx = 16
         for x, w, h, p in [(nx // 2, 3, 1, True), (nx // 3, 2, 2, True),
                            (nx // 2, 5, 1, False), (nx // 3, 6, 2.5, False)]:
@@ -53,27 +55,7 @@ class AutocorrelationTest(PyCoTestCase):
             A_ana = A_ana[0] - A_ana
             self.assertTrue(np.allclose(A, A_ana))
 
-    def test_nonuniform_impulse_autocorrelation(self):
-        x = np.array([0, 3.])
-        t = NonuniformLineScan(x, 2 * np.ones_like(x))
-        r, A = t.autocorrelation_1D(distances=np.linspace(-4, 4, 101))
-
-    def test_nonuniform_triangle_autocorrelation(self):
-        a = 0.7
-        b = 3
-        x = np.array([0, b])
-        t = NonuniformLineScan(x, a * x)
-        r, A = t.autocorrelation_1D(distances=np.linspace(0, 4, 101))
-
-        self.assertAlmostEqual(A[np.abs(r) < 1e-6][0], a ** 2 * b ** 3 / 3)
-
-        x = np.array([0, 1., 1.3, 1.7, 2.0, 2.5, 3.0])
-        t = NonuniformLineScan(x, 0.7 * x)
-        r2, A2 = t.autocorrelation_1D(distances=np.linspace(0, 4, 101))
-
-        self.assertArrayAlmostEqual(A, A2)
-
-    def test_brute_force_autocorrelation_1D(self):
+    def test_uniform_brute_force_autocorrelation_1D(self):
         n = 10
         for surf in [UniformLineScan(np.ones(n), n, periodic=False),
                      UniformLineScan(np.arange(n), n, periodic=False),
@@ -88,7 +70,7 @@ class AutocorrelationTest(PyCoTestCase):
                 dir_A[d] /= (n - d)
             self.assertArrayAlmostEqual(A, dir_A)
 
-    def test_brute_force_autocorrelation_2D(self):
+    def test_uniform_brute_force_autocorrelation_2D(self):
         n = 10
         m = 11
         for surf in [Topography(np.ones([n, m]), (n, m), periodic=False),
@@ -113,3 +95,51 @@ class AutocorrelationTest(PyCoTestCase):
             dir_A /= dir_n
             self.assertArrayAlmostEqual(A_xy, dir_A_xy)
             self.assertArrayAlmostEqual(A[:-2], dir_A[:-2])
+
+    def test_nonuniform_impulse_autocorrelation(self):
+        x = np.array([0, 3.])
+        t = NonuniformLineScan(x, 2 * np.ones_like(x))
+        r, A = _bare_autocorrelation_1D(t, distances=np.linspace(-4, 4, 101))
+
+    def test_nonuniform_triangle_autocorrelation(self):
+        a = 0.7
+        b = 3
+        x = np.array([0, b])
+        t = NonuniformLineScan(x, a * x)
+        r, A = _bare_autocorrelation_1D(t, distances=np.linspace(-4, 4, 101))
+
+        self.assertAlmostEqual(A[np.abs(r) < 1e-6][0], a ** 2 * b ** 3 / 3)
+
+        x = np.array([0, 1., 1.3, 1.7, 2.0, 2.5, 3.0])
+        t = NonuniformLineScan(x, 0.7 * x)
+        r2, A2 = _bare_autocorrelation_1D(t, distances=np.linspace(-4, 4, 101))
+
+        self.assertArrayAlmostEqual(A, A2)
+
+    def test_self_affine_uniform_autocorrelation(self):
+        r = 2048
+        s = 1
+        H = 0.8
+        slope = 0.1
+        t = fourier_synthesis((r,), (s,), H, rms_slope=slope, amplitude_distribution=lambda n: 1.0)
+
+        r, A = t.autocorrelation_1D()
+
+        m  = np.logical_and(r > 1e-3, r < 10**(-1.5))
+        b, a = np.polyfit(np.log(r[m]), np.log(A[m]), 1)
+        self.assertTrue(abs(b/2 - H) < 0.1)
+
+    def test_self_affine_nonuniform_autocorrelation(self):
+        r = 128
+        s = 1
+        H = 0.8
+        slope = 0.1
+        t = fourier_synthesis((r,), (s,), H, rms_slope=slope, amplitude_distribution=lambda n: 1.0)
+        r, A = t.autocorrelation_1D()
+        r2, A2 = t.to_nonuniform().autocorrelation_1D()
+
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.loglog(r, A, 'r-')
+        plt.loglog(r2, A2, 'k-')
+        plt.show()
