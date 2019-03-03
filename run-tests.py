bf44e935134f5fbe6f0fc713ec3378b2edc3c61d
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2019-03-03 12:30:14 +0100

    TST:forgot to add run-tests to git

---
+++ run-tests.py
@@ -0,0 +1,10 @@
+# pop the current directory from search path
+# python interpreter adds this to a top level script
+# but we will likely have a name conflict (runtests.py .vs runtests package)
+import sys; sys.path.pop(0)
+from runtests.mpi import Tester
+import os
+
+tester = Tester(os.path.join(os.path.abspath(__file__)), "PyCo")
+
+tester.main(sys.argv[1:])
