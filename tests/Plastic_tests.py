b455a136e3067003071ffe89687591422b2dd2c6
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2019-03-31 08:15:05 +0200

    TST: parallelized Plastic_test

--- tests/Plastic_tests.py
+++ tests/Plastic_tests.py
@@ -25,40 +25,68 @@
 """
 Tests plastic deformation
 """
-
 try:
     import numpy as np
-    import unittest
+    import pytest
     from scipy.optimize import bisect
     from PyCo.ContactMechanics import HardWall
     from PyCo.SolidMechanics import PeriodicFFTElasticHalfSpace
     from PyCo.Topography import read, PlasticTopography
     from PyCo.System import make_system
+    from NuMPI.Tools.ParallelNumpy import ParallelNumpy
+
+    from PyCo.Topography import Topography
+    from runtests.mpi import MPITest
     import os
+    import PyCo
 except ImportError as err:
     import sys
+
     print(err)
     sys.exit(-1)
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'file_format_examples')
 
-DATADIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'file_format_examples')
 
-# -----------------------------------------------------------------------------
-class PlasticTest(unittest.TestCase):
-    def test_hard_wall_LBFGS(self):
-        # Test that at very low hardness we converge to (almost) the bearing
-        # area geometry
-        surface = read(os.path.join(DATADIR, 'surface1.out'), format = "asc").topography()
-        system = make_system(PeriodicFFTElasticHalfSpace(surface.resolution, 1.0),
-                             HardWall(), PlasticTopography(surface, 0.0000000001))
-        offset = -0.002
-        result = system.minimize_proxy(offset=offset)
-        c = result.jac > 0.0
-        ncontact = c.sum()
+@pytest.fixture()
+def fftengine_class(comm):
+    try:
+        from FFTEngine import PFFTEngine as engine
+    except Exception as err:
+        if comm.Get_size() == 1:
+            try:
+                from FFTEngine import FFTWEngine as engine
+            except:
+                from FFTEngine import NumpyFFTEngine as engine
+        else:
+            raise err
+    return engine
 
-        bearing_area = bisect(lambda x: (surface.heights() > x).sum() - ncontact, -0.03, 0.03)
-        cba = surface.heights() > bearing_area
 
-        self.assertTrue(np.logical_not(c == cba).sum() < 25)
+def test_hard_wall_bearing_area(comm, fftengine_class, datafiles):
+    # Test that at very low hardness we converge to (almost) the bearing
+    # area geometry
+    pnp = ParallelNumpy(comm)
+    fullsurface = read(os.path.join(FIXTURE_DIR, 'surface1.out')).topography()
+    domain_resolution = fullsurface.resolution
+    substrate = PeriodicFFTElasticHalfSpace(
+        domain_resolution, 1.0,
+        fftengine=fftengine_class(domain_resolution, comm), pnp=pnp)
+    surface = Topography(fullsurface.heights(), size=domain_resolution,
+                         subdomain_location=substrate.topography_subdomain_location,
+                         subdomain_resolution=substrate.topography_subdomain_resolution,
+                         pnp=substrate.pnp)
 
-if __name__ == '__main__':
-    unittest.main()
+    system = make_system(substrate,
+                         HardWall(), PlasticTopography(surface, 0.0000000001))
+    offset = -0.002
+    result = system.minimize_proxy(offset=offset)
+    assert result.success
+    c = result.jac > 0.0
+    ncontact = pnp.sum(c)
+    bearing_area = bisect(lambda x: pnp.sum((surface.heights() > x)) - ncontact,
+                          -0.03, 0.03)
+    cba = surface.heights() > bearing_area
+    # print(comm.Get_rank())
+    assert pnp.sum(np.logical_not(c == cba)) < 25
