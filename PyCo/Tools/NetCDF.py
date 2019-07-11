28381f4ee1b01dd93d199116b4c233bc93c834f7
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-07-11 23:27:12 +0200

    MAINT: NetCDFContainer_frame -> NetCDFContainerFrame

--- PyCo/Tools/NetCDF.py
+++ PyCo/Tools/NetCDF.py
@@ -43,7 +43,7 @@ except:
 
 ###
 
-class NetCDFContainer_frame(object):
+class NetCDFContainerFrame(object):
     def __init__(self, parent, i):
         self._parent = parent
         self._i = i
@@ -343,7 +343,7 @@ class NetCDFContainer(object):
 
 
     def get_next_frame(self):
-        frame = NetCDFContainer_frame(self, self._cur_frame)
+        frame = NetCDFContainerFrame(self, self._cur_frame)
         self._cur_frame += 1
         return frame
 
@@ -397,14 +397,14 @@ class NetCDFContainer(object):
         if isinstance(i, str):
             return self.__getattr__(i)
         if isinstance(i, slice):
-            return [ NetCDFContainer_frame(self, j)
-                     for j in range(*i.indices(len(self))) ]
-        return NetCDFContainer_frame(self, i)
+            return [NetCDFContainerFrame(self, j)
+                    for j in range(*i.indices(len(self)))]
+        return NetCDFContainerFrame(self, i)
 
 
     def __iter__(self):
         for i in range(len(self)):
-            yield NetCDFContainer_frame(self, i)
+            yield NetCDFContainerFrame(self, i)
 
 
     def get_size(self):
