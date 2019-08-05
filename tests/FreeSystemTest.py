98ed993120676572de418fd9fd7998dd99cf457a
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-08-05 16:34:29 +0200

    TST: Adjust convergence criterion

--- tests/FreeSystemTest.py
+++ tests/FreeSystemTest.py
@@ -91,7 +91,7 @@ def test_minimization_simplesmoothmin(young, r_c):
     offset = .8 * S.interaction.r_c
     fun = S.objective(offset, gradient=True)
 
-    options = dict(ftol=1e-18, gtol=1e-10)
+    options = dict(ftol=1e-16, gtol=1e-8)
     disp = S.shape_minimisation_input(
         np.zeros(substrate.nb_domain_grid_pts))
 
@@ -166,7 +166,7 @@ def test_minimization(pot_class, young, base_res):
     offset = .8 * S.interaction.r_c
     fun = S.objective(offset, gradient=True)
 
-    options = dict(ftol=1e-16, gtol=1e-8)
+    options = dict(ftol=1e-18, gtol=1e-10)
     disp = S.shape_minimisation_input(
         np.zeros(substrate.nb_domain_grid_pts))
 
