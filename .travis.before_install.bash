da05e06352c1ff3edba5bdfffee2bea73fa8e0a3
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-08-07 11:19:21 +0200

    CI: Separate script for installing parallel NetCDF

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-BUILDDIR=/tmp
-
 sudo apt-get update
 sudo apt-get install libfftw3-dev
 if [ "$WITH_MPI" == "yes" ]; then
@@ -18,33 +16,6 @@ conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy
 source activate test-environment
 python -m pip install $(grep numpy requirements.txt)
 if [ "$WITH_MPI" == "yes" ]; then
-  wd=$(pwd)
-
-  curl https://parallel-netcdf.github.io/Release/pnetcdf-${PNETCDF_VERSION}.tar.gz | tar -xzC ${BUILDDIR} \
-    && cd ${BUILDDIR}/pnetcdf-${PNETCDF_VERSION} \
-    && CC=mpicc CXX=mpicxx ./configure --disable-fortran --disable-cxx --enable-shared --prefix=$HOME/.local \
-    && make \
-    && make install
-
-  curl https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz | tar -xzC ${BUILDDIR} \
-    && cd ${BUILDDIR}/hdf5-${HDF5_VERSION} \
-    && CC=mpicc CXX=mpicxx ./configure --enable-parallel --prefix=$HOME/.local \
-    && make \
-    && make install
-
-  # We need to compile NetCDF ourselves because there is no package that has
-  # parallel PnetCDF and HDF5 enabled.
-  curl https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-c-${NETCDF4_VERSION}.tar.gz | tar -xzC ${BUILDDIR} \
-    && mkdir ${BUILDDIR}/netcdf-c-build \
-    && cd ${BUILDDIR}/netcdf-c-build \
-    && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/.local -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DUSE_PARALLEL=ON -DENABLE_PARALLEL4=ON -DENABLE_PNETCDF=ON ${BUILDDIR}/netcdf-c-${NETCDF4_VERSION} \
-    && make \
-    && make install
-
-  cd $wd
   python -m pip install --no-binary mpi4py mpi4py==${MPI4PY_VERSION}
-
-  # Install netcdf4-python and make sure that it is compiled (no-binary),
-  # otherwise it will not have parallel support.
-  HDF5_DIR=$HOME/.local CC=mpicc python -m pip install --no-binary netCDF4 netCDF4==${NETCDF4_PYTHON_VERSION}
+  BUILDDIR=/tmp PREFIX=$HOME/.local source .install_parallel_netcdf.sh
 fi
