d87850ec83d3cb5282a024cc5cc68062f62c8be7
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-08-27 21:37:25 +0200

    TST: Added sphere test

--- tests/CGDugdaleTest.py
+++ tests/CGDugdaleTest.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-from PyCo.SolidMechanics import PeriodicFFTElasticHalfSpace
-from PyCo.Topography import Topography
+import pytest
+
+from PyCo.ContactMechanics import Dugdale
+from PyCo.SolidMechanics import PeriodicFFTElasticHalfSpace, FreeFFTElasticHalfSpace
+from PyCo.System import make_system
+from PyCo.Tools.Logger import screen
 from PyCo.Tools.Optimisation import constrained_conjugate_gradients
+from PyCo.Topography import Topography, make_sphere
 
 
+@pytest.mark.skip
 def test_flat():
     nx, ny = (10, 10)
     sx, sy = 1, 1
@@ -18,7 +24,27 @@ def test_flat():
                                           Dugdale=(sigma0, 1),
                                           offset=-0.5,
                                           disp0=topography.heights() + 1,
-                                          verbose=True, maxiter=50)
+                                          verbose=True, maxiter=50, logger=screen)
     assert sol.success
 
     assert (- sol.jac == sigma0).all
+
+
+def test_sphere():
+    nx, ny = (256, 256)
+    sx, sy = (2 * np.pi, 2 * np.pi)
+
+    halfspace = FreeFFTElasticHalfSpace((nx, ny), 10, (sx, sx))
+    interaction = Dugdale(1, 0.2)
+    topography = make_sphere(10, (nx, ny), (sx, sy))
+    system = make_system(halfspace, interaction, topography)
+
+    for offset in [0.0, 0.1, 0.2]:
+        opt = system.minimize_proxy(
+            verbose=True,
+            maxiter=50,
+            prestol=1e-4,
+            offset=offset
+        )
+        assert opt.success
+
