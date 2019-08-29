89e6c8845f7aa22f98dbf5da8da94e9791cc2742
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-08-29 09:37:19 +0200

    TST: Added Dugdale test

--- tests/CGDugdaleTest.py
+++ tests/CGDugdaleTest.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import pytest
 
+import PyCo.ReferenceSolutions.MaugisDugdale as MD
 from PyCo.ContactMechanics import Dugdale
 from PyCo.SolidMechanics import PeriodicFFTElasticHalfSpace, FreeFFTElasticHalfSpace
 from PyCo.System import make_system
@@ -30,21 +31,90 @@ def test_flat():
     assert (- sol.jac == sigma0).all
 
 
-def test_sphere():
+def test_sphere(plot=False):
     nx, ny = (256, 256)
-    sx, sy = (2 * np.pi, 2 * np.pi)
+    sx, sy = (8, 8)
 
-    halfspace = FreeFFTElasticHalfSpace((nx, ny), 10, (sx, sx))
-    interaction = Dugdale(1, 0.2)
-    topography = make_sphere(10, (nx, ny), (sx, sy))
+    sphere_radius = 10
+    contact_modulus = 10
+    cohesive_stress = 1
+    Dugdale_length = 0.2
+    work_of_adhesion = cohesive_stress * Dugdale_length
+
+    displacement_factor = (np.pi**2 * work_of_adhesion**2 * sphere_radius / (4/3 * contact_modulus)**2)**(1/3)
+    area_factor = np.pi * (np.pi * work_of_adhesion * sphere_radius**2 / (4/3 * contact_modulus))**(2/3)
+    force_factor = np.pi * work_of_adhesion * sphere_radius
+
+    halfspace = FreeFFTElasticHalfSpace((nx, ny), contact_modulus, (sx, sx))
+    interaction = Dugdale(cohesive_stress, Dugdale_length)
+    topography = make_sphere(sphere_radius, (nx, ny), (sx, sy))
     system = make_system(halfspace, interaction, topography)
 
-    for offset in [0.0, 0.1, 0.2]:
+    if plot:
+        import matplotlib.pyplot as plt
+
+    displacements = [ -0.15, -0.05, 0.0, 0.05, 0.15]
+    forces = []
+    areas = []
+    for displacement in displacements:
         opt = system.minimize_proxy(
             verbose=True,
-            maxiter=50,
-            prestol=1e-4,
-            offset=offset
+            maxiter=100,
+            prestol=1e-5,
+            offset=displacement
         )
-        assert opt.success
+        forces += [opt.jac.sum()]
+        areas += [opt.active_set.sum() * halfspace.area_per_pt]
+
+        if plot:
+            plt.figure()
+            plt.title('offset = ${}$'.format(displacement))
+            plt.plot(opt.jac[nx//2], 'k-')
+        # Some of these do not converge
+        #assert opt.success
+        print('Displacement = {}, number of iterations = {}, converged = {}'.format(displacement, opt.nit, opt.success))
+
+    displacements = np.array(displacements) / displacement_factor
+    forces = np.array(forces) / force_factor
+    areas = np.array(areas) / area_factor
+
+    if plot:
+        md_areas = np.linspace(0.1, 25, 101)
+        md_forces, md_displacements = MD.load_and_displacement(np.sqrt(md_areas / np.pi), sphere_radius,
+                                                               contact_modulus, work_of_adhesion, cohesive_stress)
+
+        md_areas /= area_factor
+        md_forces /= force_factor
+        md_displacements /= displacement_factor
+
+        plt.figure()
+        plt.subplot(221)
+        plt.plot(md_forces, md_areas, 'k-')
+        plt.plot(forces, areas, 'ro')
+        plt.xlabel('Normalized force $F/\pi w R$')
+        plt.ylabel('Normalized area $A/(\pi w R^2/K)^{2/3}$')
+        plt.subplot(222)
+        plt.plot(md_displacements, md_areas, 'k-')
+        plt.plot(displacements, areas, 'ro')
+        plt.xlabel('Normalized displacement $\delta/(\pi^2 w^2 R/K^2)^{1/3}$')
+        plt.ylabel('Normalized area $A/(\pi w R^2/K)^{2/3}$')
+        plt.subplot(223)
+        plt.plot(md_displacements, md_forces, 'k-')
+        plt.plot(displacements, forces, 'ro')
+        plt.xlabel('Normalized displacement $\delta/(\pi^2 w^2 R/K^2)^{1/3}$')
+        plt.ylabel('Normalized force $F/\pi w R$')
+        plt.tight_layout()
+        plt.show()
+
+    md_forces, md_displacements = MD.load_and_displacement(np.sqrt(areas / np.pi), sphere_radius,
+                                                           contact_modulus, work_of_adhesion, cohesive_stress)
+    md_forces /= force_factor
+    md_displacements /= displacement_factor
+
+    print('Force error: ', np.std(md_forces - forces))
+    print('Displacement error: ', np.std(md_displacements - displacements))
+
+    assert np.std(md_forces - forces) < 0.35
+    assert np.std(md_displacements - displacements) < 0.35
+
 
