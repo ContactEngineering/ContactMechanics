BUG: Fixed computation of F matrix

PyCo/SolidMechanics/GreensFunctions/Anisotropic.py
@@ -81,9 +81,9 @@ class AnisotropicGreensFunction(object):
         # Q = 2: det(M) = c0 + 4*c2 + 16*c4 + 64*c6
         # Q = 3: det(M) = c0 + 9*c2 + 81*c4 + 729*c6
 
-        fac1 = 0.7
-        fac2 = 1.0
-        fac3 = 1.3
+        fac1 = 1
+        fac2 = 2
+        fac3 = 3
         A = np.array([[0, 0, 0, 1],
                       [fac1 ** 6, fac1 ** 4, fac1 ** 2, 1],
                       [fac2 ** 6, fac2 ** 4, fac2 ** 2, 1],
@@ -122,13 +122,12 @@ class AnisotropicGreensFunction(object):
     def make_F(self, qx, qy, qz, eta):
         q = [(qx, qy, _qz) for _qz in qz]
         F = np.zeros((3, len(qz)), dtype=complex)
-        for k, alpha, l in np.ndindex(3, len(qz), 3):
-            F[k, alpha] += self.elasticity_tensor(alpha, 2, k, l) * q[alpha][k] * eta[alpha][l]
+        for i, k, alpha, l in np.ndindex(3, 3, len(qz), 3):
+            F[i, alpha] += 1j * self.elasticity_tensor(i, 2, k, l) * q[alpha][k] * eta[alpha][l]
         return F
 
     def make_U_and_F(self, qx, qy):
-        evs = self.find_eigenvalues(qx, qy)
-        qz = [1j * Q if Q > 0 else -1j * Q for Q in evs]
+        qz = -1j * self.find_eigenvalues(qx, qy)
         eta = self.find_eigenvectors(qx, qy, qz)
         return self.make_U(qz, eta), self.make_F(qx, qy, qz, eta)
 

examples/plot_greens_function.py
@@ -32,8 +32,10 @@ from PyCo.SolidMechanics.GreensFunctions import AnisotropicGreensFunction, Isotr
 
 C11 = 1
 C44 = 0.3
-C12 = C11 - 2 * C44 + 0.01  # 0.3
-aniso = AnisotropicGreensFunction(C11, C12, C44)
+C12 = C11 - 2 * C44 + 0.3 # + 0.0001  # 0.3
+aniso1 = AnisotropicGreensFunction(C11, C12, C44)
+#C12 = C11 - 2 * C44 + 0.3  # 0.3
+#aniso2 = AnisotropicGreensFunction(C11, C12, C44)
 
 nu = 0.3
 E = 2 * C44 * (1 + nu)
@@ -41,7 +43,7 @@ iso = IsotropicGreensFunction(C44, nu)
 
 special_points = [('$\Gamma$', (0., 0.)), ('$M$', (0.5, 0.5)), ('$X$', (0.5, 0.)), ('$\Gamma$', (0., 0.))]
 colors = ['r', 'g', 'b', 'k']
-labels = ['isotropic', 'anisotropic']
+labels = ['isotropic', 'anisotropic1', 'anisotropic2']
 
 npts = 100
 z = 0.0
@@ -49,7 +51,8 @@ for (sym1, (qx1, qy1)), (sym2, (qx2, qy2)) in zip(special_points[:-1], special_p
     print(sym1, sym2)
     qx = 2 * np.pi * np.linspace(qx1, qx2, npts)
     qy = 2 * np.pi * np.linspace(qy1, qy2, npts)
-    for color, label, gf in zip(colors, labels, [iso, aniso]):
+    #plt.plot(np.linspace(z, z + 1.0, npts), C44*4*(1-nu)*np.sqrt(qx**2+qy**2)/(3-4*nu), 'k-', lw=4)
+    for color, label, gf in zip(colors, labels, [iso, aniso1]):
         phi = gf.stiffness(qx, qy)
         phizz = np.real(phi[:, 2, 2])
         plt.plot(np.linspace(z, z + 1.0, npts), phizz, color + '-', label=label)