a7a228b29db7f409ff6c62c046720a8e8daaaa70
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2019-12-24 10:06:13 +0100

    ENH: fourier_synthesis provide self-affine prefactor in fourier_synthesis

--- tests/Topography/GenerationTests.py
+++ tests/Topography/GenerationTests.py
@@ -19,3 +19,29 @@ def test_fourier_synthesis(n):
     qy, psdy = topography.transpose().power_spectrum_1D()
 
     assert psdy[-1] < 10 * psdx[-1] # assert psdy is not much bigger
+
+def test_fourier_synthesis_c0():
+
+    H=0.7
+    c0 = 1.
+
+    n=256
+    s=n*4.
+    ls = 4
+    qs = 2 * np.pi / ls
+    np.random.seed(0)
+    topography = fourier_synthesis((n, n), (s, s),
+                                   H,
+                                   c0=c0,
+                                   long_cutoff=s/2,
+                                   short_cutoff=ls,
+                                   amplitude_distribution=lambda n: np.ones(n)
+                                   )
+    ref_slope = np.sqrt(1 / (4 * np.pi) * c0 /(1-H) * qs**(2-2*H))
+    assert abs(topography.rms_slope() - ref_slope) /  ref_slope < 1e-1
+
+    #import matplotlib.pyplot as plt
+
+    #plt.loglog(*topography.power_spectrum_1D())
+
+    #plt.show(block=True)
