756e5d0e87ed4ed5363defceefbeec91e3f451c3
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-01-20 14:46:31 +0100

    MAINT: PEP8

--- tests/Topography/GenerationTests.py
+++ tests/Topography/GenerationTests.py
@@ -2,7 +2,8 @@ from PyCo.Topography.Generation import fourier_synthesis
 import pytest
 import numpy as np
 
-@pytest.mark.parametrize("n", [128,129])
+
+@pytest.mark.parametrize("n", [128, 129])
 def test_fourier_synthesis(n):
     H = 0.74
     rms_slope = 1.2
@@ -18,27 +19,27 @@ def test_fourier_synthesis(n):
     qx, psdx = topography.power_spectrum_1D()
     qy, psdy = topography.transpose().power_spectrum_1D()
 
-    assert psdy[-1] < 10 * psdx[-1] # assert psdy is not much bigger
+    assert psdy[-1] < 10 * psdx[-1]  # assert psdy is not much bigger
+
 
 def test_fourier_synthesis_c0():
-
-    H=0.7
+    H = 0.7
     c0 = 1.
 
-    n=512
-    s=n*4.
+    n = 512
+    s = n * 4.
     ls = 8
     qs = 2 * np.pi / ls
     np.random.seed(0)
     topography = fourier_synthesis((n, n), (s, s),
                                    H,
                                    c0=c0,
-                                   long_cutoff=s/2,
+                                   long_cutoff=s / 2,
                                    short_cutoff=ls,
                                    amplitude_distribution=lambda n: np.ones(n)
                                    )
-    ref_slope = np.sqrt(1 / (4 * np.pi) * c0 /(1-H) * qs**(2-2*H))
-    assert abs(topography.rms_slope() - ref_slope) /  ref_slope < 1e-1
+    ref_slope = np.sqrt(1 / (4 * np.pi) * c0 / (1 - H) * qs ** (2 - 2 * H))
+    assert abs(topography.rms_slope() - ref_slope) / ref_slope < 1e-1
 
     if False:
         import matplotlib.pyplot as plt
@@ -46,7 +47,7 @@ def test_fourier_synthesis_c0():
 
         fig, ax = plt.subplots()
         ax.loglog(q, psd, label="generated data")
-        ax.loglog(q, c0 * q**(-2-2*H), label=r"$c_0 q^{-2-2H}$")
+        ax.loglog(q, c0 * q ** (-2 - 2 * H), label=r"$c_0 q^{-2-2H}$")
 
         ax.set_xlabel("q")
         ax.set_ylabel(r"$C^{iso}$")
@@ -56,7 +57,7 @@ def test_fourier_synthesis_c0():
         q, psd = topography.power_spectrum_1D()
         fig, ax = plt.subplots()
         ax.loglog(q, psd, label="generated data")
-        ax.loglog(q, c0/np.pi * q**(-1-2*H), label=r"$c_0 q^{-1-2H}$")
+        ax.loglog(q, c0 / np.pi * q ** (-1 - 2 * H), label=r"$c_0 q^{-1-2H}$")
 
         ax.legend()
         ax.set_xlabel("q")
