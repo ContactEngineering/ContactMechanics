1daf473470b7839613d66a6ec374e1bdc2b15a23
Michael Röttger <info@michael-roettger.de>
2020-02-19 16:35:47 +0100

    WIP: Fixed test for changed DI orientation (related to #295).

--- tests/Topography/SurfaceTests.py
+++ tests/Topography/SurfaceTests.py
@@ -912,10 +912,11 @@ def test_di_orientation():
     # Check values in 4 corners, this should fix orientation
     #
     di_heights = di_t.heights()
-    assert pytest.approx(di_heights[0,0], abs=1e-3) == 6.060
-    assert pytest.approx(di_heights[0, -1], abs=1e-3) == 2.843
-    assert pytest.approx(di_heights[-1, 0], abs=1e-3) == -9.740
-    assert pytest.approx(di_heights[-1, -1], abs=1e-3) == -30.306
+    assert pytest.approx(di_heights[0, 0], abs=1e-3) == 2.843
+    assert pytest.approx(di_heights[0,-1], abs=1e-3) == 6.060
+    assert pytest.approx(di_heights[-1, 0], abs=1e-3) == -30.306
+    assert pytest.approx(di_heights[-1, -1], abs=1e-3) == -9.740
+
 
 
 class ibwSurfaceTest(unittest.TestCase):
