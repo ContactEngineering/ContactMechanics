a08c0c3c63dccf704b0b45004a553f8ce4e42da0
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2020-06-01 22:30:33 +0200

    MAINT: Update license header should use '--follow' option of 'git log'

--- maintenance/replace_header.py
+++ maintenance/replace_header.py
@@ -1,17 +1,18 @@
 #
-# 
+# Copyright 2020 Lars Pastewka
+#
 # ### MIT license
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,7 +33,7 @@ while file_lines[0].startswith('#'):
 
 file_lines.insert(0, '#\n')
 for header_line in header_lines[::-1]:
-    file_lines.insert(0, '# {}'.format(header_line))
+    file_lines.insert(0, '# {}'.format(header_line).strip() + '\n')
 file_lines.insert(0, '#\n')
 
 open(sys.argv[1], 'w').writelines(file_lines)
