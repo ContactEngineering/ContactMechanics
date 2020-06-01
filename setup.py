27a60f89f7b1cc082e540e708be004e37efb7e20
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2020-06-01 22:11:51 +0200

    MAINT: Updated setup.py and requirements

--- setup.py
+++ setup.py
@@ -27,102 +27,23 @@
 import glob
 import re
 
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 from setuptools.command.build_ext import build_ext
 
 
-class CustomBuildExtCommand(build_ext):
-    """build_ext command for use when numpy headers are needed."""
-
-    def run(self):
-        # Import numpy here, only when headers are needed
-        import numpy
-
-        # Add numpy headers to include_dirs
-        self.include_dirs.append(numpy.get_include())
-
-        # Call original build_ext command
-        build_ext.run(self)
-
-
-#
-# Optain LAPACK configuration from numpy
-#
-
-try:
-    import numpy as np
-except ImportError:
-    print("WARNING: Could not find numpy, skipping LAPACK detection.")
-    np = None
-
-lib_dirs = []
-libs = []
-extra_link_args = []
-extra_objects = []
-if np is not None:
-    for k, v in np.__config__.__dict__.items():
-        if re.match('lapack_.*_info', k):
-            if v:
-                print("* Using LAPACK information from '{}' dictionary in " \
-                    "numpy.__config__".format(k))
-                try:
-                    print("    library_dirs = {}".format(v['library_dirs']))
-                    lib_dirs += v['library_dirs']
-                except:
-                    print("    No 'library_dirs' entry found.")
-                try:
-                    print("    libraries = {}".format(v['libraries']))
-                    libs += v['libraries']
-                except:
-                    print("    No 'libraries' entry found.")
-                try:
-                    print("    extra_link_args = '{}'".format(v['extra_link_args']))
-                    extra_link_args += v['extra_link_args']
-                except:
-                    print("    No 'extra_link_args' entry found.")
-                # We use whichever lapack_*_info comes first, hopefully there is
-                # just one.
-                break
-    if len(libs) == 0:
-        # No entries from np.__config__
-        print("* Using lapack_lite")
-        extra_objects += [glob.glob(np.linalg.__path__[0]+'/lapack_lite*.so')[0]]
-
-
-extra_compile_args = ["-std=c++11"]
-print(extra_objects)
-
 scripts = [
-   'commandline/ContactMechanics/hard_wall.py',
-   'commandline/Adhesion/soft_wall.py',
-   'commandline/ContactMechanics/plotacf.py',
-   'commandline/ContactMechanics/plotpsd.py',
-   'commandline/ContactMechanics/plotmap.py'
+   'commandline/hard_wall.py',
+   'commandline/plotacf.py',
+   'commandline/plotpsd.py',
+   'commandline/plotmap.py'
    ]
 
-extensions = [
-    Extension(
-        name='_PyCo',
-        sources=['c/autocorrelation.cpp',
-                 'c/bicubic.cpp',
-                 'c/patchfinder.cpp',
-                 'c/PyCo_module.cpp'],
-        extra_compile_args=extra_compile_args,
-        library_dirs=lib_dirs,
-        libraries=libs,
-        extra_link_args=extra_link_args,
-        extra_objects=extra_objects
-    )
-]
-
 setup(
-    name="PyCo",
-    cmdclass={'build_ext': CustomBuildExtCommand},
+    name="ContactMechanics",
     scripts=scripts,
     packages=find_packages(),
     package_data={'': ['ChangeLog.md']},
     include_package_data=True,
-    ext_modules=extensions,
     # metadata for upload to PyPI
     author="Lars Pastewka",
     author_email="lars.pastewka@imtek.uni-freiburg.de",
@@ -132,13 +53,14 @@ setup(
     # dependencies
     python_requires='>=3.5.0',
     use_scm_version=True,
-    zip_safe=False,
+    zip_safe=True,False
     setup_requires=[
         'setuptools_scm>=3.5.0'
     ],
     install_requires=[
         'numpy>=1.11.0',
         'NuMPI',
-        'muFFT'
+        'muFFT',
+        'SurfaceTopography'
     ]
 )
