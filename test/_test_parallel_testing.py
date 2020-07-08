9e46b136848f21c3b6a12800a56dbcf087ea6a30
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-07-08 15:14:47 +0200

    MAINT: PEP8

--- test/_test_parallel_testing.py
+++ test/_test_parallel_testing.py
@@ -1,4 +1,3 @@
-
 """
 This is a demo for checking how run-tests behaves when not all ranks fail.
 The test will not deadlock, but the ranks that succeeded during one test will
@@ -11,10 +10,12 @@ Parallelising assertions is hence not mandatory but recommended.
 from NuMPI.Tools import Reduction
 import numpy as np
 
+
 def test_parallel_failure(comm):
     # will this test fail properly or just deadlock ?
     assert comm.rank < 1
     # NICE it doesn't deadlock !
+
 
 #        z is not a typo, it is for making the test execute after the upper one
 def test_z_continue_another_test(comm):
@@ -22,6 +23,7 @@ def test_z_continue_another_test(comm):
     pnp = Reduction(comm)
     pnp.sum(np.array([3, 4]))
     assert True
+
 
 def test_a_clean_way(comm):
     pnp = Reduction(comm)
