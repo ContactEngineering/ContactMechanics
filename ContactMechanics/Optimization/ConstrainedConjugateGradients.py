532b001ec14bf10fcf7654517cb2b6e66f92ba10
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-11-11 14:34:34 +0100

    BUG: use normal array not masked array for the unmasked part of the surface

--- ContactMechanics/Optimization/ConstrainedConjugateGradients.py
+++ ContactMechanics/Optimization/ConstrainedConjugateGradients.py
@@ -171,10 +171,13 @@ def constrained_conjugate_gradients(substrate, topography, hardness=None,
     else:
         comp_mask[tuple(comp_slice)][surf_mask] = False
         surf_mask = np.logical_not(surf_mask)
+
+    masked_surface = np.asarray(surface[surf_mask])
+
     pad_mask = np.logical_not(comp_mask)
     N_pad = reduction.sum(pad_mask * 1)
-    u_r[comp_mask] = np.where(u_r[comp_mask] < surface[surf_mask] + offset,
-                              surface[surf_mask] + offset,
+    u_r[comp_mask] = np.where(u_r[comp_mask] < masked_surface + offset,
+                              masked_surface + offset,
                               u_r[comp_mask])
 
     result = optim.OptimizeResult()
@@ -223,7 +226,7 @@ def constrained_conjugate_gradients(substrate, topography, hardness=None,
         A_cg = reduction.sum(c_r * 1)
 
         # Compute gap
-        g_r = u_r[comp_mask] - surface[surf_mask]
+        g_r = u_r[comp_mask] - masked_surface
         if external_force is not None:
             offset = 0
             if A_cg > 0:
@@ -364,7 +367,7 @@ def constrained_conjugate_gradients(substrate, topography, hardness=None,
         else:
             rms_pen = sqrt(G)
         max_pen = max(0.0,
-                      reduction.max(c_r[comp_mask] * (surface[surf_mask] +
+                      reduction.max(c_r[comp_mask] * (masked_surface +
                                                       offset -
                                                       u_r[comp_mask])))
         result.maxcv = {"max_pen": max_pen,
