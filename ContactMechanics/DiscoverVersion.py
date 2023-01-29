8014081a43a76be239457ab4c01338d1b574460b
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-01-29 13:53:20 +0100

    BUG: We need to check version via git last; this is because otherwise we may find the version of whatevery downstream package we are currently developing inside its own git directory

--- ContactMechanics/DiscoverVersion.py
+++ ContactMechanics/DiscoverVersion.py
@@ -39,6 +39,7 @@
 # - If this fails, ask pkg_resources
 #
 
+import os
 import subprocess
 
 
@@ -66,10 +67,16 @@ def get_version_from_git():
     """
     Discover version from git repository.
     """
-    git_describe = subprocess.run(
-        ['git', 'describe', '--tags', '--dirty', '--always'],
-        stdout=subprocess.PIPE)
-    if git_describe.returncode != 0:
+    if not os.path.exists('.git'):
+        raise CannotDiscoverVersion('.git subdirectory does not exist.')
+
+    try:
+        git_describe = subprocess.run(
+            ['git', 'describe', '--tags', '--dirty', '--always'],
+            stdout=subprocess.PIPE)
+    except FileNotFoundError:
+        git_describe = None
+    if git_describe is None or git_describe.returncode != 0:
         raise CannotDiscoverVersion('git execution failed.')
     version = git_describe.stdout.decode('latin-1').strip()
 
@@ -87,39 +94,39 @@ def get_version_from_git():
     return version
 
 
+_pkg_name = __name__.replace('.DiscoverVersion', '')
+
+# importlib is present in Python >= 3.8
 try:
-    __version__ = get_version_from_git()
-except CannotDiscoverVersion:
+    from importlib.metadata import version
+
+    __version__ = version(_pkg_name)
+except ImportError:
     __version__ = None
-
-if __version__ is None:
-    try:
-        __version__ = get_version_from_pkg_info()
-    except CannotDiscoverVersion:
-        __version__ = None
-
-# Not sure the mechanisms below are much different from parsing PKG-INFO...
-
-pkg_name = __name__.replace('.DiscoverVersion', '')
-if __version__ is None:
-    # importlib is present in Python >= 3.8
-    try:
-        from importlib.metadata import version
-
-        __version__ = version(pkg_name)
-    except ImportError:
-        __version__ = None
 
 if __version__ is None:
     # pkg_resources is part of setuptools
     try:
         from pkg_resources import get_distribution
 
-        __version__ = get_distribution(pkg_name).version
+        __version__ = get_distribution(_pkg_name).version
     except ImportError:
+        __version__ = None
+
+# Not sure this mechanisms below is much different from the above two
+if __version__ is None:
+    try:
+        __version__ = get_version_from_pkg_info()
+    except CannotDiscoverVersion:
+        __version__ = None
+
+if __version__ is None:
+    try:
+        __version__ = get_version_from_git()
+    except CannotDiscoverVersion:
         __version__ = None
 
 # Nope. Out of options.
 
 if __version__ is None:
-    raise CannotDiscoverVersion('Tried git, PKG-INFO, importlib and pkg_resources')
+    raise CannotDiscoverVersion('Tried importlib, pkg_resources, PKG-INFO and git')
