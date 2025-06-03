9a96cb791a9fe5668404335389ec58bbf89d4495
sannant <asanner@ethz.ch>
2025-06-03 17:56:17 +0200

    WIP: Comparing primal vs. dual on Hertz contact problem. Dual is 3 times faster

--- test/test_numpi_ccgs.py
+++ test/test_numpi_ccgs.py
@@ -4,6 +4,8 @@ import pytest
 
 from NuMPI.Optimization import CCGWithoutRestart, CCGWithRestart
 from SurfaceTopography import make_sphere
+from matplotlib import pyplot as plt
+
 import ContactMechanics as Solid
 
 
@@ -197,7 +199,7 @@ def test_dual_obj_nonperiodic():
 
     offset = 0.005
     # lbounds = np.zeros((2 * nx, 2 * ny))
-
+    #
     # Inpose contact constraint only where we have topopgraphy, i.e. not in the padding region
     # lbounds = np.ma.masked_all(system.substrate.nb_subdomain_grid_pts)
     # lbounds.mask[system.substrate.local_topography_subdomain_slices] = False
@@ -241,10 +243,35 @@ def test_dual_obj_nonperiodic():
     gap_bugnicourt = fun(res.x)[1]
     gap_bugnicourt = gap_bugnicourt.reshape((nx, ny))
     #
-    # # ##################POLONSKY-KEER#####################################
+    # ##################POLONSKY-KEER#####################################
+    #
+    # class WRAPOBJ():
+    #     def __init__(self, ):
+    #         self.original_fun = system.dual_objective(offset, gradient=True)
+    #         fig, [self.ax, self.axpres] = plt.subplots(2, 1)
+    #         # plt.show()
+    #         self.ax.set_yscale('log')
+    #         self.it = 0
+    #
+    #     def __call__(self, x):
+    #         en, grad = self.original_fun(x)
+    #         rms_grad = np.sqrt(system.reduction.sum((grad * (x > 0)) ** 2))
+    #         print(rms_grad)
+    #         self.ax.plot(self.it, rms_grad, ".")
+    #         self.it += 1
+    #         self.ax.relim()
+    #         self.axpres.clear()
+    #         self.axpres.imshow(x.reshape((nx, ny)))
+    #         plt.draw()
+    #         plt.pause(0.01)
+    #         return en, grad
+
     res = CCGWithRestart.constrained_conjugate_gradients(
         system.dual_objective(offset, gradient=True),
-        system.dual_hessian_product, init_pressure, gtol=gtol)
+        # WRAPOBJ(),
+        system.dual_hessian_product,
+        init_pressure + 0.01, # This algorithm gets stuck if the initial pressure is perfectly 0
+        gtol=gtol)
     assert res.success
 
     polonsky_force = res.x.reshape((nx, ny))
@@ -263,17 +290,21 @@ def test_dual_obj_nonperiodic():
     np.testing.assert_allclose(lbfgsb_force, bugnicourt_force, atol=1e-3)
     np.testing.assert_allclose(bugnicourt_force, polonsky_force, atol=1e-3)
 
+
+    # assert False
+
     # ##########TEST MEAN VALUES#######################################
     mean_val = np.mean(lbfgsb_force)
     print('mean {}'.format(mean_val))
     # ####################POLONSKY-KEER##############################
     res = CCGWithRestart.constrained_conjugate_gradients(
         system.dual_objective(offset, gradient=True),
-        system.dual_hessian_product, init_pressure, gtol=gtol,
+        system.dual_hessian_product, init_pressure + 0.01, gtol=gtol,
         mean_value=mean_val)
 
     assert res.success
     polonsky_mean = res.x.reshape((nx, ny))
+
 
     # # ####################BUGNICOURT###################################
     CCGWithoutRestart.constrained_conjugate_gradients(
@@ -286,3 +317,94 @@ def test_dual_obj_nonperiodic():
 
     np.testing.assert_allclose(polonsky_mean, lbfgsb_force, atol=1e-3)
     np.testing.assert_allclose(bugnicourt_mean, lbfgsb_force, atol=1e-3)
+#
+#
+# def test_primal_obj_nonperiodic():
+#     # Note that there is a bit of overlap with test_ccg_without_restart_nonperiodic
+#
+#     nx = ny = 128
+#     sx, sy = 1., 1.
+#     R = 10.
+#
+#     gtol = 1e-6
+#
+#     surface = make_sphere(R, (nx, ny), (sx, sy), kind="paraboloid")
+#     Es = 50.
+#     substrate = Solid.FreeFFTElasticHalfSpace((nx, ny), young=Es,
+#                                                   physical_sizes=(sx, sy))
+#
+#     system = Solid.Systems.NonSmoothContactSystem(substrate, surface)
+#
+#     offset = 0.005
+#
+#     lbounds = np.ma.masked_all(self.substrate.nb_subdomain_grid_pts)
+#     lbounds.mask[self.substrate.local_topography_subdomain_slices] = False
+#     lbounds[self.substrate.local_topography_subdomain_slices] = self.surface.heights() + offset
+#
+#     lbounds.set_fill_value(-np.inf)
+#
+#     init_gap =
+#     disp = np.zeros((nx, ny))
+#     init_gap[substrate.topography_subdomain_slices] = disp - surface.heights() - offset
+#
+#
+#
+#
+#     # ####################POLONSKY-KEER##############################
+#     res = CCGWithRestart.constrained_conjugate_gradients(
+#         system.primal_objective(offset, gradient=True),
+#         system.primal_hessian_product, x0=init_gap, gtol=gtol)
+#
+#     assert res.success
+#     polonsky_gap = res.x.reshape((nx, ny))
+#
+#     # ####################BUGNICOURT###################################
+#     res = CCGWithoutRestart.constrained_conjugate_gradients(
+#         system.primal_objective(offset, gradient=True),
+#         system.primal_hessian_product, x0=init_gap, mean_val=None, gtol=gtol)
+#     assert res.success
+#
+#     bugnicourt_gap = res.x.reshape((nx, ny))
+#
+#     # #####################LBFGSB#####################################
+#     res = optim.minimize(system.primal_objective(offset, gradient=True),
+#                          system.shape_minimisation_input(init_gap),
+#                          method='L-BFGS-B', jac=True,
+#                          bounds=bnds,
+#                          options=dict(gtol=gtol, ftol=1e-20))
+#
+#     assert res.success
+#     lbfgsb_gap = res.x.reshape((nx, ny))
+#
+#     np.testing.assert_allclose(polonsky_gap, bugnicourt_gap, atol=1e-3)
+#     np.testing.assert_allclose(polonsky_gap, lbfgsb_gap, atol=1e-3)
+#     np.testing.assert_allclose(lbfgsb_gap, bugnicourt_gap, atol=1e-3)
+#
+#     # ##########TEST MEAN VALUES#######################################
+#     mean_val = np.mean(lbfgsb_gap)
+#     # ####################POLONSKY-KEER##############################
+#     res = CCGWithRestart.constrained_conjugate_gradients(
+#         system.primal_objective(offset, gradient=True),
+#         system.primal_hessian_product, init_gap, gtol=gtol,
+#         mean_value=mean_val)
+#
+#     assert res.success
+#     polonsky_gap_mean_cons = res.x.reshape((nx, ny))
+#
+#     # ####################BUGNICOURT###################################
+#     CCGWithoutRestart.constrained_conjugate_gradients(system.primal_objective
+#                                                       (offset, gradient=True),
+#                                                       system.
+#                                                       primal_hessian_product,
+#                                                       x0=init_gap,
+#                                                       mean_val=mean_val,
+#                                                       gtol=gtol
+#                                                       )
+#     assert res.success
+#
+#     bugnicourt_gap_mean_cons = res.x.reshape((nx, ny))
+#
+#     np.testing.assert_allclose(polonsky_gap_mean_cons, lbfgsb_gap, atol=1e-3)
+#     np.testing.assert_allclose(bugnicourt_gap_mean_cons, lbfgsb_gap, atol=1e-3)
+#     np.testing.assert_allclose(lbfgsb_gap, bugnicourt_gap, atol=1e-3)
+#     np.testing.assert_allclose(lbfgsb_gap, bugnicourt_gap_mean_cons, atol=1e-3)
