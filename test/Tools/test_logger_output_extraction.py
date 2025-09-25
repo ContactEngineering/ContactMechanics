72407b611be5fe1449ea68085599740faa0bf603
sannant <asanner@ethz.ch>
2025-09-25 09:13:33 +0200

    TST: make dummy logfile findable by pytest

--- test/Tools/test_logger_output_extraction.py
+++ test/Tools/test_logger_output_extraction.py
@@ -1,8 +1,11 @@
+import os
 from ContactMechanics.Tools.Logger import read_convergence_log
 
 
 def test_read_sample_logfile():
-    data = read_convergence_log("dummy_logger_output.log", )
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    log_file = os.path.join(test_dir, "dummy_logger_output.log")
+    data = read_convergence_log(log_file)
 
     for key in ['energy', 'force', 'frac_rep_area', 'frac_att_area', 'max_residual',
                 'rms_residual']:
